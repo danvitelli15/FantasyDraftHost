1d3714ab63dfca264ce5bef632cd4429cc4a6162
Daniel Vitelli <DVitelli@rbaconsulting.com>
2019-10-28 16:38:04 -0500

    Building player panel component

--- Web/PlayerPanel.js
+++ Web/PlayerPanel.js
@@ -1,10 +1,24 @@
+const element = React.createElement
+
 class PlayerPanel extends React.Component {
     render() {
-        return ('div', null, 'hello world');
+        return ('div', null, element(PlayerBasics, null), element(PlayerDetails, null));
+    }
+}
+
+class PlayerBasics extends React.Component {
+    render() {
+        return ('div', null, 'player basic information');
+    }
+}
+
+class PlayerDetails extends React.Component {
+    render() {
+        return ('div', null, 'player stats information');
     }
 }
 
 ReactDOM.render(
-    React.createElement(PlayerPanel),
+    element(PlayerPanel),
     document.querySelector('#PlayerPanel')
 );
